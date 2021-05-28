20dc1e70ce8cc2c6911d5b5f3b9011a2a18dc4c7
emilorn <emmikall95@gmail.com>
2021-05-28 09:31:01 +0000

    Added comments

--- ReadingAPITest/DataBaseHandler/DatabaseUpdater.py
+++ ReadingAPITest/DataBaseHandler/DatabaseUpdater.py
@@ -10,12 +10,12 @@ my_database = Database("air_quality_data")
 
 
 def upload_latest_to_db(database):
-    loftgaedi_response = requests.get("https://api.ust.is/aq/a/getLatest")
-    loftgaedi_content = json.loads(loftgaedi_response.content)
+    loftgaedi_content = json.loads(requests.get("https://api.ust.is/aq/a/getLatest").content)
     for sensor_name in loftgaedi_content.keys():
         found_sensor = database.get_one({"local_id": sensor_name})
         parameter_keys = list(loftgaedi_content[sensor_name]["parameters"].keys())
         if found_sensor is None:
+            # adding a new sensor to the system
             for key in parameter_keys:
                 key_copy = key
                 number_of_measurements = len(loftgaedi_content[sensor_name]["parameters"][key_copy].keys()) - 2
@@ -28,6 +28,7 @@ def upload_latest_to_db(database):
 
                 measurements = dict()
                 for measurement_index in range(0, number_of_measurements):
+                    # measurement index is loftgaedis "0", "1",...
                     measurement_content = loftgaedi_content[sensor_name]["parameters"][key_copy][
                         str(measurement_index)]
                     measurement_date = measurement_content["endtime"]
@@ -36,11 +37,13 @@ def upload_latest_to_db(database):
                         "value": measurement_content["value"],
                         "verification": measurement_content["verification"]
                     }
+                    # fixing the format so the key is the date instead of "0" or "1" etc
                     del loftgaedi_content[sensor_name]["parameters"][key_copy][str(measurement_index)]
                 loftgaedi_content[sensor_name]["parameters"][key_copy]["measurements"] = measurements
             database.insert(loftgaedi_content[sensor_name])
 
         else:
+            # if the sensor exists, check for duplicates
             for key in parameter_keys:
                 key_copy = key
                 number_of_measurements = len(loftgaedi_content[sensor_name]["parameters"][key_copy].keys()) - 2
@@ -51,13 +54,13 @@ def upload_latest_to_db(database):
                         loftgaedi_content[sensor_name]["parameters"][key]
                     del loftgaedi_content[sensor_name]["parameters"][key]
 
-                # TODO: what if key copy does not exist in found sensor? D:
-
                 if key_copy in found_sensor["parameters"].keys():
                     found_sensor_measurements = found_sensor["parameters"][key_copy]["measurements"]
                     for measurement_index in range(0, number_of_measurements):
+                        # measurement index is loftgaedis "0", "1",...
                         measurement_data = loftgaedi_content[sensor_name]["parameters"][key_copy][str(measurement_index)]
                         if measurement_data["endtime"] not in found_sensor_measurements.keys():
+                            # new measurement found
                             found_sensor_measurements[measurement_data["endtime"]] = {
                                 "value": measurement_data["value"],
                                 "verification": measurement_data["verification"]
